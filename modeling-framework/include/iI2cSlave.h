0e1e21514066220e811aa2de3f5a053a48d6e662
inbarmoskovich <32809763+inbarmoskovich@users.noreply.github.com>
2018-04-18 13:41:26 +0300

    temp

---
+++ modeling-framework/include/iI2cSlave.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <functional>
+
+typedef bool ack;
+
+class iI2cSlaveBaseReadStreamV1 {
+  public:
+    /// Sends an ACK and then reads a byte to \p rx_buffer.
+    /// Repeats \p size times or until a STOP signal was received from the master.
+    /// \param rx_buffer Buffer to hold data from the I2C master.
+    /// \param size Maximum bytes to be read into \p rx_buffer.
+    /// \return Number or bytes read into \p rx_buffer.
+    ///         If less than \p size, the corresponding instance of iI2cSlaveBaseReadStream is no longer active and the write callback should return immediately.
+    virtual size_t ReadAndSendAck(uint8_t *rx_buffer, size_t size) = 0;
+
+//    /// Sends a NACK signal to the master.
+//    /// After calling this function, the corresponding instance of iI2cSlaveBaseReadStream is no longer active and the write callback should return immediately.
+//    virtual void SendNack() = 0;
+
+//    /// Sends a ACK signal to the master.
+//    virtual void SendAck() = 0;
+
+//    /// \return true if a NACK signal was received during the current transaction, otherwise returns false.
+//    virtual bool ReceivedNack() = 0;
+
+    /// \return true if a stop signal was received during the current transaction, otherwise returns false.
+    virtual bool ReceivedStop() = 0;
+};
+
+class iI2cSlaveBaseWriteStreamV1 {
+  public:
+    /// Sends data to the I2C master.
+    /// \param tx_buffer A buffer that holds the data to be sent to the I2C master.
+    /// \param size Maximum number of bytes to be sent.
+    /// \return Number of bytes written to the I2C master.
+    ///         If less than \p size, a NACK was received from the master.
+    virtual size_t Write(uint8_t* tx_buffer, size_t size) = 0;
+
+//    /// Sends a NACK signal to the master.
+//    /// Should only be used once on callback entry.
+//    /// After calling this function, the corresponding instance of iI2cSlaveBaseReadStream is no longer active and the write callback should return immediately.
+//    virtual void SendNack() = 0;
+//
+//    /// Sends a ACK signal to the master.
+//    /// Should only be used once on callback entry.
+//    virtual void SendAck() = 0;
+
+    /// \return true if a NACK was received during the current transaction, otherwise returns false.
+    virtual bool ReceivedNack() = 0;
+};
+
+typedef iI2cSlaveBaseReadStreamV1 iI2cSlaveBaseReadStream;
+typedef iI2cSlaveBaseWriteStreamV1 iI2cSlaveBaseWriteStream;
+
+/// Called by an instance of iSpiSlave when a the I2C master writes to the slave.
+/// Receives a iI2cSlaveBaseReadStream to read data from.
+/// If this function returns without setting the last ACK/NACK, an NACK signal will be assumed and sent to the master for the remaining bytes.
+typedef std::function<void(uint8_t, iI2cSlaveBaseReadStream&)> i2c_write_command_callback_t;
+
+/// Called by an instance of iSpiSlave when a the I2C master reads from the slave.
+/// Receives a iI2cSlaveBaseWriteStream.
+typedef std::function<void(uint8_t, iI2cSlaveBaseWriteStream&)> i2c_read_command_callback_t;
+
+class iI2cSlaveBaseV1 {
+  public:
+    /// Sets a callback to be called when the I2C master is attempting to write to the slave.
+    /// \param callback
+    virtual void SetMasterWriteCommandCallback(i2c_write_command_callback_t callback) = 0;
+
+    /// Sets a callback to be called when the I2C master is attempting to read from the slave.
+    /// \param callback
+    virtual void SetMasterReadCommandCallback(i2c_read_command_callback_t callback) = 0;
+
+    virtual void SetSlaveAddress(uint8_t address, uint8_t address_mask) = 0;
+};
+
+typedef iI2cSlaveBaseV1 iI2cSlave;
